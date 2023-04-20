e56e7577f2d4ed2f7f4041d6a169a14e6a9796fd
raj8679 <devraj8679@gmail.com>
2023-04-20 09:58:25 +0530

    added pagination and product remove functionality

--- Javascript/Coffee-app/scripts/dashboard.js
+++ Javascript/Coffee-app/scripts/dashboard.js
@@ -52,33 +52,70 @@ document
 // display data in table code below
 function displayData(data) {
   document.querySelector("tbody").innerHTML = null;
-  data.forEach((el) => {
-    let tr = document.createElement("tr");
-    let td1 = document.createElement("td");
-    td1.innerText = el.name;
-    let td2 = document.createElement("td");
-    td2.innerText = el.coffeeType;
-    let td3 = document.createElement("td");
-    td3.innerText = el.size;
-    let td4 = document.createElement("td");
-    td4.innerText = el.quantity;
-    let td5 = document.createElement("td");
-    td5.innerText = el.price;
-    let editBtn = document.createElement("button");
-    editBtn.innerText = "Edit";
-    editBtn.style.backgroundColor = "tomato";
-    editBtn.style.color = "white";
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
-    deleteBtn.style.backgroundColor = "red";
-    deleteBtn.style.color = "white";
-    deleteBtn.addEventListener("click", function (event) {
-      event.target.parentNode.remove();
-    });
+  data.forEach((el, i) => {
+    if (i <= 9) {
+      let tr = document.createElement("tr");
+      let td1 = document.createElement("td");
+      td1.innerText = el.name;
+      let td2 = document.createElement("td");
+      td2.innerText = el.coffeeType;
+      let td3 = document.createElement("td");
+      td3.innerText = el.size;
+      let td4 = document.createElement("td");
+      td4.innerText = el.quantity;
+      let td5 = document.createElement("td");
+      td5.innerText = el.price;
+      let editBtn = document.createElement("button");
+      editBtn.innerText = "Edit";
+      editBtn.style.backgroundColor = "tomato";
+      editBtn.style.color = "white";
+      let deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "Delete";
+      deleteBtn.style.backgroundColor = "red";
+      deleteBtn.style.color = "white";
+      deleteBtn.addEventListener("click", function (event) {
+        deleteFunc(i);
+        event.target.parentNode.remove();
+      });
 
-    tr.append(td1, td2, td3, td4, td4, td5, editBtn, deleteBtn);
-    document.querySelector("tbody").append(tr);
+      tr.append(td1, td2, td3, td4, td4, td5, editBtn, deleteBtn);
+      document.querySelector("tbody").append(tr);
+    }
   });
 }
 displayData(orderDetailLS);
 // ***********************
+
+// delete function code below
+const deleteFunc = (i) => {
+  let filteredData = orderDetailLS.filter((el, ind) => {
+    return ind !== i;
+  });
+  localStorage.setItem("orderDetailLS", JSON.stringify(filteredData));
+};
+// *****************************
+
+// pagination code below
+let pageCount = 1;
+const paginationFunc = (val) => {
+  if (val === "inc") {
+    pageCount++;
+  } else if (val === "dec") {
+    pageCount--;
+  }
+  document.getElementById("currentBtn").innerText = pageCount;
+  let paginatedData = orderDetailLS.filter((el, i) => {
+    return i > (pageCount - 1) * 10 && i <= pageCount * 10;
+  });
+  console.log(paginatedData);
+  displayData(paginatedData);
+};
+
+document
+  .getElementById("nextBtn")
+  .addEventListener("click", () => paginationFunc("inc"));
+document
+  .getElementById("prevBtn")
+  .addEventListener("click", () => paginationFunc("dec"));
+
+// ********************************
